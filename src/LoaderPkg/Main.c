08de8ac6d1c970c35318fcbdfc7a3855c99ab798
tomon9086 <tomon9086@gmail.com>
2021-04-26 15:13:36 +0900

    format code

--- src/LoaderPkg/Main.c
+++ src/LoaderPkg/Main.c
@@ -1,8 +1,9 @@
-#include  <Uefi.h>
-#include  <Library/UefiLib.h>
+#include <Library/UefiLib.h>
+#include <Uefi.h>
 
-EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
+                           EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello, World!\n");
-  while (1);
+  while (1) continue;
   return EFI_SUCCESS;
 }
