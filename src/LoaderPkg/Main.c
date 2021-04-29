c2414e0103b3e2f84267434ca548efed0bd544e1
tomon9086 <tomon9086@gmail.com>
2021-04-29 17:51:28 +0900

    error handling

--- src/LoaderPkg/Main.c
+++ src/LoaderPkg/Main.c
@@ -35,76 +35,107 @@ EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
 
 EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
                    EFI_GRAPHICS_OUTPUT_PROTOCOL **gop) {
+  EFI_STATUS status;
   UINTN num_gop_handles = 0;
   EFI_HANDLE *gop_handles = NULL;
-  (gBS->LocateHandleBuffer)(ByProtocol,
-                            &gEfiGraphicsOutputProtocolGuid,
-                            NULL,
-                            &num_gop_handles,
-                            &gop_handles);
+  status = (gBS->LocateHandleBuffer)(ByProtocol,
+                                     &gEfiGraphicsOutputProtocolGuid,
+                                     NULL,
+                                     &num_gop_handles,
+                                     &gop_handles);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
-  (gBS->OpenProtocol)(gop_handles[0],
-                      &gEfiGraphicsOutputProtocolGuid,
-                      (VOID **)gop,
-                      image_handle,
-                      NULL,
-                      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  status = (gBS->OpenProtocol)(gop_handles[0],
+                               &gEfiGraphicsOutputProtocolGuid,
+                               (VOID **)gop,
+                               image_handle,
+                               NULL,
+                               EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
   FreePool(gop_handles);
-
   return EFI_SUCCESS;
 }
 
 EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
+  EFI_STATUS status;
   EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
   EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
 
-  (gBS->OpenProtocol)(image_handle,
-                      &gEfiLoadedImageProtocolGuid,
-                      (VOID **)&loaded_image,
-                      image_handle,
-                      NULL,
-                      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  status = (gBS->OpenProtocol)(image_handle,
+                               &gEfiLoadedImageProtocolGuid,
+                               (VOID **)&loaded_image,
+                               image_handle,
+                               NULL,
+                               EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
-  (gBS->OpenProtocol)(loaded_image->DeviceHandle,
-                      &gEfiSimpleFileSystemProtocolGuid,
-                      (VOID **)&fs,
-                      image_handle,
-                      NULL,
-                      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  status = (gBS->OpenProtocol)(loaded_image->DeviceHandle,
+                               &gEfiSimpleFileSystemProtocolGuid,
+                               (VOID **)&fs,
+                               image_handle,
+                               NULL,
+                               EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
-  fs->OpenVolume(fs, root);
-
-  return EFI_SUCCESS;
+  return fs->OpenVolume(fs, root);
 }
 
 EFI_STATUS CallKernel(EFI_HANDLE image_handle, CHAR16 *path) {
+  EFI_STATUS status;
   EFI_FILE_PROTOCOL *root_dir;
   EFI_FILE_PROTOCOL *file;
 
-  OpenRootDir(image_handle, &root_dir);
-  root_dir->Open(
+  status = OpenRootDir(image_handle, &root_dir);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
+  status = root_dir->Open(
       root_dir,
       &file,
       path,
       EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE,
       0);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
   UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
   UINT8 file_info_buffer[file_info_size];
-  file->GetInfo(file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+  status =
+      file->GetInfo(file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
   EFI_FILE_INFO *file_info = (EFI_FILE_INFO *)file_info_buffer;
   UINTN file_size = file_info->FileSize;
   EFI_PHYSICAL_ADDRESS base_addr = 0x100000;
-  (gBS->AllocatePages)(
+  status = (gBS->AllocatePages)(
       AllocateAddress, EfiLoaderData, (file_size + 0xfff) / 0x1000, &base_addr);
-  file->Read(file, &file_size, (VOID *)base_addr);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
+  status = file->Read(file, &file_size, (VOID *)base_addr);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
   UINT64 entry_addr = *(UINT64 *)(base_addr + 24);
 
   EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
-  OpenGOP(image_handle, &gop);
+  status = OpenGOP(image_handle, &gop);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
   typedef void EntryPointType(UINT64, UINT64);
   EntryPointType *entry_point = (EntryPointType *)entry_addr;
@@ -115,8 +146,7 @@ EFI_STATUS CallKernel(EFI_HANDLE image_handle, CHAR16 *path) {
 
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
                            EFI_SYSTEM_TABLE *system_table) {
-  Print(L"Hello, World!\n");
-
+  EFI_STATUS status;
   CHAR8 memMapBuff[4096 * 4];
   struct MemoryMap memMap = {
       sizeof(memMapBuff),
@@ -126,11 +156,18 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
       0,
   };
 
-  GetMemoryMap(&memMap);
+  status = GetMemoryMap(&memMap);
+  if (EFI_ERROR(status)) {
+    Print(L"%r\n", status);
+    Halt();
+  }
 
-  CallKernel(image_handle, L"\\kernel.elf");
+  status = CallKernel(image_handle, L"\\kernel.elf");
+  if (EFI_ERROR(status)) {
+    Print(L"%r\n", status);
+    Halt();
+  }
 
-  Print(L"Done!\n");
   Halt();
   return EFI_SUCCESS;
 }
