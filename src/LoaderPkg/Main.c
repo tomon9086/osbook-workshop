71b388be4683e49f333559b846fe314d95f052e2
tomon9086 <tomon9086@gmail.com>
2021-04-29 11:22:58 +0900

    calling kernel

--- src/LoaderPkg/Main.c
+++ src/LoaderPkg/Main.c
@@ -1,5 +1,7 @@
+#include <Guid/FileInfo.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
+#include <Protocol/LoadedImage.h>
 #include <Uefi.h>
 
 struct MemoryMap {
@@ -25,6 +27,61 @@ EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
                              &map->descriptorVersion);
 }
 
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
+  EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
+
+  (gBS->OpenProtocol)(image_handle,
+                      &gEfiLoadedImageProtocolGuid,
+                      (VOID **)&loaded_image,
+                      image_handle,
+                      NULL,
+                      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  (gBS->OpenProtocol)(loaded_image->DeviceHandle,
+                      &gEfiSimpleFileSystemProtocolGuid,
+                      (VOID **)&fs,
+                      image_handle,
+                      NULL,
+                      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  fs->OpenVolume(fs, root);
+
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS CallKernel(EFI_HANDLE image_handle, CHAR16 *path) {
+  EFI_FILE_PROTOCOL *root_dir;
+  EFI_FILE_PROTOCOL *file;
+
+  OpenRootDir(image_handle, &root_dir);
+  root_dir->Open(
+      root_dir,
+      &file,
+      path,
+      EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE,
+      0);
+
+  UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+  UINT8 file_info_buffer[file_info_size];
+  file->GetInfo(file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+
+  EFI_FILE_INFO *file_info = (EFI_FILE_INFO *)file_info_buffer;
+  UINTN file_size = file_info->FileSize;
+  EFI_PHYSICAL_ADDRESS base_addr = 0x100000;
+  (gBS->AllocatePages)(
+      AllocateAddress, EfiLoaderData, (file_size + 0xfff) / 0x1000, &base_addr);
+  file->Read(file, &file_size, (VOID *)base_addr);
+
+  UINT64 entry_addr = *(UINT64 *)(base_addr + 24);
+
+  typedef void EntryPointType(void);
+  EntryPointType *entry_point = (EntryPointType *)entry_addr;
+  entry_point();
+
+  return EFI_SUCCESS;
+}
+
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
                            EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello, World!\n");
@@ -40,14 +97,9 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
 
   GetMemoryMap(&memMap);
 
-  // メモリ領域の大きさ 設定と実際の値
-  Print(L"%08lx\n", memMap.bufferSize);
-  Print(L"%08lx\n", memMap.memoryMapSize);
-  // メモリ領域の種別
-  Print(L"%08lx\n", ((EFI_MEMORY_DESCRIPTOR *)memMap.buffer)->Type);
-  // メモリ領域のページ (4KiB単位) 数
-  Print(L"%08lx\n", ((EFI_MEMORY_DESCRIPTOR *)memMap.buffer)->NumberOfPages);
+  CallKernel(image_handle, L"\\kernel.elf");
 
+  Print(L"Done!\n");
   while (1) continue;
   return EFI_SUCCESS;
 }
