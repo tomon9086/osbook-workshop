56f02450901e77c7d1cda4ad2df85f5ef81d3611
tomon9086 <tomon9086@gmail.com>
2021-05-01 00:30:39 +0900

    pixel writer

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,46 +1,36 @@
 #include <cstddef>
 #include <cstdint>
+#include <new>
 
 #include "frame_buffer_config.hpp"
+#include "pixel_writer.hpp"
 
-struct PixelColor {
-  uint8_t r, g, b;
-};
-
-int WritePixel(const FrameBufferConfig& config, int x, int y,
-               const PixelColor& c) {
-  const int pixel_position = config.pixels_per_scan_line * y + x;
-  uint8_t* p = &config.frame_buffer[4 * pixel_position];
-
-  switch (config.pixel_format) {
-    case kPixelRGBReserved8BitPerColor:
-      p[0] = c.r;
-      p[1] = c.g;
-      p[2] = c.b;
-      return 0;
-
-    case kPixelBGRReserved8BitPerColor:
-      p[0] = c.b;
-      p[1] = c.g;
-      p[2] = c.r;
-      return 0;
-
-    default:
-      return -1;
-  }
-}
-
-void ClearFrame(const FrameBufferConfig& frame_buffer_config) {
+void ClearFrame(const FrameBufferConfig& frame_buffer_config,
+                PixelWriter* pixel_writer) {
   for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
     for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
       struct PixelColor color = {255, 255, 255};
-      WritePixel(frame_buffer_config, x, y, color);
+      pixel_writer->Write(x, y, color);
     }
   }
 }
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
-  ClearFrame(frame_buffer_config);
+  char pixel_writer_buf[sizeof(RGBReserved8BitPerColorPixelWriter)];
+  PixelWriter* pixel_writer;
+
+  switch (frame_buffer_config.pixel_format) {
+    case kPixelRGBReserved8BitPerColor:
+      pixel_writer = new (pixel_writer_buf)
+          RGBReserved8BitPerColorPixelWriter(frame_buffer_config);
+      break;
+    case kPixelBGRReserved8BitPerColor:
+      pixel_writer = new (pixel_writer_buf)
+          BGRReserved8BitPerColorPixelWriter(frame_buffer_config);
+      break;
+  }
+
+  ClearFrame(frame_buffer_config, pixel_writer);
 
   int w = 300;
   int h = 200;
@@ -48,7 +38,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     for (int y = 0; y < h; ++y) {
       struct PixelColor color = {
           (uint8_t)(255 * x / w), 180, (uint8_t)(255 * y / h)};
-      WritePixel(frame_buffer_config, 100 + x, 100 + y, color);
+      pixel_writer->Write(100 + x, 100 + y, color);
     }
   }
 
