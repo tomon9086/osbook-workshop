8c1fde1b994ed5aeeb30c5691ab36062e39cb648
tomon9086 <tomon9086@gmail.com>
2021-06-16 00:40:26 +0900

    efficientify clear_frame

--- src/kernel/src/main.rs
+++ src/kernel/src/main.rs
@@ -28,15 +28,6 @@ fn panic_handler(_: &PanicInfo) -> ! {
   halt()
 }
 
-fn clear_frame(frame_buffer_config: &mut FrameBufferConfig, pixel_writer: &dyn PixelWriter) {
-  for x in 0..frame_buffer_config.horizontal_resolution {
-    for y in 0..frame_buffer_config.vertical_resolution {
-      let color = PixelColor::new(255, 255, 255);
-      (*pixel_writer).write(frame_buffer_config, x, y, &color);
-    }
-  }
-}
-
 #[no_mangle]
 pub extern "C" fn KernelMain(frame_buffer_config: &mut FrameBufferConfig) {
   let pixel_writer: &dyn PixelWriter;
@@ -50,12 +41,12 @@ pub extern "C" fn KernelMain(frame_buffer_config: &mut FrameBufferConfig) {
     }
   }
 
-  clear_frame(frame_buffer_config, pixel_writer);
+  (*pixel_writer).clear_frame(frame_buffer_config, &PixelColor::new(255, 255, 255));
 
   let w = 300;
   let h = 200;
-  for x in 0..w {
-    for y in 0..h {
+  for y in 0..h {
+    for x in 0..w {
       let color = PixelColor::new((255 * x / w) as u8, 180, (255 * y / h) as u8);
       (*pixel_writer).write(frame_buffer_config, 100 + x, 100 + y, &color);
     }
--- src/kernel/src/pixel_writer.rs
+++ src/kernel/src/pixel_writer.rs
@@ -9,12 +9,37 @@ pub trait PixelWriter {
     }
   }
 
+  fn clear_frame(&self, config: &mut FrameBufferConfig, color: &PixelColor);
   fn write(&self, config: &mut FrameBufferConfig, x: u32, y: u32, color: &PixelColor);
 }
 
 pub struct RGBReserved8BitPerColorPixelWriter {}
 
 impl PixelWriter for RGBReserved8BitPerColorPixelWriter {
+  fn clear_frame(&self, config: &mut FrameBufferConfig, color: &PixelColor) {
+    unsafe {
+      let r = color.r();
+      let g = color.g();
+      let b = color.b();
+      let p_frame_buffer = config.frame_buffer as usize;
+
+      let mut y = 0;
+      while y < config.vertical_resolution {
+        let mut x = 0;
+        while x < config.horizontal_resolution {
+          let p = p_frame_buffer + ((config.pixels_per_scan_line * y + x) << 2) as usize;
+
+          *((p + 0) as *mut u8) = r;
+          *((p + 1) as *mut u8) = g;
+          *((p + 2) as *mut u8) = b;
+          
+          x += 1;
+        }
+        y += 1;
+      }
+    }
+  }
+
   fn write(&self, config: &mut FrameBufferConfig, x: u32, y: u32, color: &PixelColor) {
     unsafe {
       let p = self.pixel_at(config, x, y);
@@ -28,6 +53,30 @@ impl PixelWriter for RGBReserved8BitPerColorPixelWriter {
 pub struct BGRReserved8BitPerColorPixelWriter {}
 
 impl PixelWriter for BGRReserved8BitPerColorPixelWriter {
+  fn clear_frame(&self, config: &mut FrameBufferConfig, color: &PixelColor) {
+    unsafe {
+      let r = color.r();
+      let g = color.g();
+      let b = color.b();
+      let p_frame_buffer = config.frame_buffer as usize;
+
+      let mut y = 0;
+      while y < config.vertical_resolution {
+        let mut x = 0;
+        while x < config.horizontal_resolution {
+          let p = p_frame_buffer + ((config.pixels_per_scan_line * y + x) << 2) as usize;
+
+          *((p + 0) as *mut u8) = b;
+          *((p + 1) as *mut u8) = g;
+          *((p + 2) as *mut u8) = r;
+
+          x += 1;
+        }
+        y += 1;
+      }
+    }
+  }
+
   fn write(&self, config: &mut FrameBufferConfig, x: u32, y: u32, color: &PixelColor) {
     unsafe {
       let p = self.pixel_at(config, x, y);
