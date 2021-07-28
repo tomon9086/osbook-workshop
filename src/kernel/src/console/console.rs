f920ee0a2eba6d6e62c95d8efa54fb9017d27609
tomon9086 <tomon9086@gmail.com>
2021-07-28 19:42:59 +0900

    build-std

--- src/kernel/src/console/console.rs
+++ src/kernel/src/console/console.rs
@@ -1,28 +1,11 @@
-use core::clone::Clone;
-use core::marker::Copy;
 use crate::console::print::*;
 use crate::graphics::{PixelWriter, PixelColor};
 
 const COLS: u32 = 80;
 const ROWS: u32 = 25;
 
-struct Buffer<'b>(pub [&'b mut [char; COLS as usize]; ROWS as usize]);
-
-// impl Copy for Buffer<'_> {}
-
-impl Clone for Buffer<'_> {
-  fn clone(&self) -> Self {
-    let mut b = [&mut [0 as char; COLS as usize]; ROWS as usize];
-    for y in 0..(ROWS as usize) {
-      let mut r = [0 as char; COLS as usize];
-      for x in 0..(COLS as usize) {
-        r[x] = self.0[y][x];
-      }
-      b[y] = &mut r;
-    }
-    Self(b)
-  }
-}
+// #[derive(Clone)]
+// struct Buffer<'b>(pub [&'b mut [char; COLS as usize]; ROWS as usize]);
 
 pub struct Console<'c> {
   pixel_writer: &'c dyn PixelWriter,
@@ -30,9 +13,6 @@ pub struct Console<'c> {
   rows: u32,
   cursor_col: u32,
   cursor_row: u32,
-  // buffer: &'c [&'c str; ROWS as usize],
-  // buffer: &'c mut [&'c mut [char; COLS as usize]; ROWS as usize],
-  buffer: &'c Buffer<'c>,
 }
 
 impl Console<'_> {
@@ -43,9 +23,6 @@ impl Console<'_> {
       rows: ROWS,
       cursor_col: 0,
       cursor_row: 0,
-      // buffer: &[""; ROWS as usize],
-      // buffer: &mut [&mut [0 as char; COLS as usize]; ROWS as usize],
-      buffer: &Buffer([&mut [0 as char; COLS as usize]; ROWS as usize]),
     }
   }
 
@@ -70,7 +47,7 @@ impl Console<'_> {
     let y = self.cursor_row * char_height;
 
     write_ascii(self.pixel_writer, x, y, c, &PixelColor::new(0, 0, 0));
-    self.buffer.0[y as usize][x as usize] = c;
+    // self.buffer.0[y as usize][x as usize] = c;
   }
 
   fn newline(&mut self) {
@@ -78,15 +55,15 @@ impl Console<'_> {
     if self.cursor_row < self.rows - 1 {
       self.cursor_row += 1;
     } else {
-      self.pixel_writer.clear_frame(&PixelColor::new(255, 255, 255));
-      let mut b = [&mut [0 as char; COLS as usize]; ROWS as usize];
-      for i in 1..(ROWS as usize) {
-        b[i - 1] = self.buffer.0[i];
-        for c in self.buffer.0[i] {
-          self.put_char(*c);
-        }
-      }
-      self.buffer.0 = b;
+      // self.pixel_writer.clear_frame(&PixelColor::new(255, 255, 255));
+      // let mut b = [&mut [0 as char; COLS as usize]; ROWS as usize];
+      // for i in 1..(ROWS as usize) {
+      //   b[i - 1] = self.buffer.0[i];
+      //   for c in self.buffer.0[i] {
+      //     self.put_char(*c);
+      //   }
+      // }
+      // self.buffer.0 = b;
     }
   }
 }
--- src/kernel/src/graphics/frame_buffer.rs
+++ src/kernel/src/graphics/frame_buffer.rs
@@ -1,31 +1,14 @@
-use core::clone::Clone;
-
+#[derive(Clone)]
 pub enum PixelFormat {
   KPixelRGBReserved8BitPerColor,
   KPixelBGRReserved8BitPerColor,
 }
 
+#[derive(Clone)]
 pub struct FrameBufferConfig {
   pub frame_buffer: *mut u8,
   pub pixels_per_scan_line: u32,
   pub horizontal_resolution: u32,
   pub vertical_resolution: u32,
   pub pixel_format: PixelFormat,
-}
-
-impl Clone for FrameBufferConfig {
-  fn clone(&self) -> Self {
-    unsafe {
-      FrameBufferConfig {
-        frame_buffer: &mut (*self.frame_buffer),
-        pixels_per_scan_line: self.pixels_per_scan_line,
-        horizontal_resolution: self.horizontal_resolution,
-        vertical_resolution: self.vertical_resolution,
-        pixel_format: match self.pixel_format {
-          PixelFormat::KPixelRGBReserved8BitPerColor => PixelFormat::KPixelRGBReserved8BitPerColor,
-          PixelFormat::KPixelBGRReserved8BitPerColor => PixelFormat::KPixelBGRReserved8BitPerColor,
-        },
-      }
-    }
-  }
 }
